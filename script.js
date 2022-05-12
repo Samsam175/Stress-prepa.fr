44f1c4193e0f35aa1b155e3307c2c09d6a7efd4c
Sammy A <66476300+Samsam175@users.noreply.github.com>
2022-05-12 17:32:24 +0200

    Mise a jour auto sur desktop

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 (function () {
   const second = 1000,
 		cent =100,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
     concours = "Apr 19, 2023 08:00:00",
     resccmp = "Jun 08, 2022 18:00:00",
@@ -15,50 +15,64 @@
     countDown4 = new Date(resx).getTime(),
 	  daterentree = new Date("Sep 1, 2022 08:00:00").getTime(),
 	  
-      x = setInterval(function() {    
+    x = setInterval(function() {    
 
-        now = new Date().getTime(),
-            distance = countDown - now,
-            distance2 = countDown2 - now,
-            distance3 = countDown3 - now,
-            distance4 = countDown4 - now,
-			tpspasse = now - daterentree,
+      now = new Date().getTime(),
+      
+      distance = countDown - now,
+      distance2 = countDown2 - now,
+      distance3 = countDown3 - now,
+      distance4 = countDown4 - now,
+			
+      tpspasse = now - daterentree,
 			percena = (tpspasse / (distance+tpspasse) )*cent 
+      
       if (percena < 0) { percena = 0 }
       if (distance2 < 0) { distance2 = 0 }
-      if (distance3 < 0) { distance3 = 0 }
       if (distance4 < 0) { distance4 = 0 }
-			
-      document.getElementById("days2").innerText = Math.floor(distance2 / (day)),
-          document.getElementById("hours2").innerText = Math.floor((distance2 % (day)) / (hour)),
-          document.getElementById("minutes2").innerText = Math.floor((distance2 % (hour)) / (minute)),
-          document.getElementById("seconds2").innerText = Math.floor((distance2 % (minute)) / second),
-          document.getElementById("days4").innerText = Math.floor(distance4 / (day)),
-          document.getElementById("hours4").innerText = Math.floor((distance4 % (day)) / (hour)),
-          document.getElementById("minutes4").innerText = Math.floor((distance4 % (hour)) / (minute)),
-          document.getElementById("seconds4").innerText = Math.floor((distance4 % (minute)) / second),
-          document.getElementById("days3").innerText = Math.floor(distance3 / (day)),
-          document.getElementById("hours3").innerText = Math.floor((distance3 % (day)) / (hour)),
-          document.getElementById("minutes3").innerText = Math.floor((distance3 % (hour)) / (minute)),
-          document.getElementById("seconds3").innerText = Math.floor((distance3 % (minute)) / second), 
-		  document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-		  document.getElementById("percent").innerText = percena.toFixed(6) ,
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+      
+      if (distance3 > 0) { 
+        resteannee = document.getElementById("resteannee"),
+        resteannee.style.display = "none",
 
-        //do something later when date is reached
-        if (distance3 < 0) {
-          let headline = document.getElementById("headline"),
-              countdown = document.getElementById("countdown"),
-              content = document.getElementById("content");
+        document.getElementById("days2").innerText = Math.floor(distance2 / (day)),
+        document.getElementById("hours2").innerText = Math.floor((distance2 % (day)) / (hour)),
+        document.getElementById("minutes2").innerText = Math.floor((distance2 % (hour)) / (minute)),
+        document.getElementById("seconds2").innerText = Math.floor((distance2 % (minute)) / second),
+        document.getElementById("days4").innerText = Math.floor(distance4 / (day)),
+        document.getElementById("hours4").innerText = Math.floor((distance4 % (day)) / (hour)),
+        document.getElementById("minutes4").innerText = Math.floor((distance4 % (hour)) / (minute)),
+        document.getElementById("seconds4").innerText = Math.floor((distance4 % (minute)) / second),
+        document.getElementById("days3").innerText = Math.floor(distance3 / (day)),
+        document.getElementById("hours3").innerText = Math.floor((distance3 % (day)) / (hour)),
+        document.getElementById("minutes3").innerText = Math.floor((distance3 % (hour)) / (minute)),
+        document.getElementById("seconds3").innerText = Math.floor((distance3 % (minute)) / second),
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+		    document.getElementById("percent").innerText = percena.toFixed(6) ,
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-		headline.innerText = "C'est trop tard !!! \n Et puisse le sort vous etre favorable";
-          countdown.style.display = "none";
-          content.style.display = "block";
+      }
+        //apres la fin des résultats
+      else {
+          document.getElementById("days0").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours0").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes0").innerText = Math.floor((distance % (hour)) / (minute)),
+		      document.getElementById("percent0").innerText = percena.toFixed(6) ,
+          document.getElementById("seconds0").innerText = Math.floor((distance % (minute)) / second),
 
-          clearInterval(x);
+          attenteres  = document.getElementById("attenteres"),
+          attenterescountdown  = document.getElementById("attenterescountdown"),
+          attenteres.style.display = "none",
+          attenterescountdown .style.display = "none",
+
+          document.getElementById("days0").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours0").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes0").innerText = Math.floor((distance % (hour)) / (minute)),
+		      document.getElementById("percent0").innerText = percena.toFixed(6) ,
+          document.getElementById("seconds0").innerText = Math.floor((distance % (minute)) / second); 
+
         }
-        //seconds
       }, 0)
   }());
